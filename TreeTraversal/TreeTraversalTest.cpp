c28747b3b8181ba50874fea56080b129fde4e84b
Andre Edwards <aedwards000@gmail.com>
2013-02-04 14:20:23 -0500

    Updated main class.

--- TreeTraversal/TreeTraversalTest.cpp
+++ TreeTraversal/TreeTraversalTest.cpp
@@ -4,15 +4,18 @@ void main() {
 
 	Tree* myTree = new Tree();
 
-
+	Tree* otherTree = new Tree();
+	otherTree->addNode(45);
+	otherTree->addNode(17);
+	otherTree->addNode(96);
 	
-	myTree->addNode(3);
-	myTree->addNode(1);
-	myTree->addNode(2);
-	myTree->addNode(4);
-	myTree->addNode(5);
-	myTree->addNode(9);
-	myTree->addNode(7);
+	myTree->addNode(47);
+	myTree->addNode(30);
+	myTree->addNode(15);
+	myTree->addNode(12);
+
+	myTree->Root()->setRight(otherTree->Root());	//adds otherTree to myTree
+
 	
 
 	cout << "Root node: " << myTree->Root()->Key() << endl;
